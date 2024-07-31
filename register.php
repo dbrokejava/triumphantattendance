22bce6cd446e911d2251be2730839f9ec737519c
dbrokejava <167309199+dbrokejava@users.noreply.github.com>
2024-08-01 00:10:34 +0200

    Update register.php

--- register.php
+++ register.php
@@ -3,11 +3,24 @@
 
 header('Content-Type: application/json');
 
-// Generate registration options
-echo json_encode([
+// Generate a challenge and other registration options
+$options = [
     'challenge' => base64_encode(random_bytes(32)),
-    'rp' => ['name' => 'Your Application'],
-    'user' => ['id' => base64_encode(random_bytes(16)), 'name' => 'user@example.com', 'displayName' => 'User'],
-    'pubKeyCredParams' => [['type' => 'public-key', 'alg' => -7]]
-]);
+    'rp' => ['name' => 'Triumphant College Boot Camp'],
+    'user' => [
+        'id' => base64_encode(random_bytes(16)),
+        'name' => 'user@example.com',
+        'displayName' => 'User'
+    ],
+    'pubKeyCredParams' => [
+        ['type' => 'public-key', 'alg' => -7]
+    ],
+    'authenticatorSelection' => [
+        'authenticatorAttachment' => 'platform',
+        'requireResidentKey' => false,
+        'userVerification' => 'preferred'
+    ]
+];
+
+echo json_encode($options);
 ?>
